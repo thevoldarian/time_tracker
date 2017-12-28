455de23e1ff30626de149eee1ec287a643eb9fe5
Raymond Beauchamp <thevoldarian@gmail.com>
2017-12-28 22:30:15 +0000

    update authentication;

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -15,6 +15,8 @@ describe 'navigate' do
   end
   describe 'creation' do
     before do 
+      user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Snow")
+      login_as(user, :scope => :user)
       visit new_post_path
     end
     it 'has a new form that can be reached' do
@@ -26,5 +28,13 @@ describe 'navigate' do
       click_on "Save"
       expect(page).to have_content("Some Rationale")
     end
+    
+    it 'will have a user associated with it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
+      click_on "Save"
+      expect(User.last.posts.last.rationale).to eq("User Association")
+    end
+    
   end
 end
